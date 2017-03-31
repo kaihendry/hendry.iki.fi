444c6886c2b78c6c6ecaee78c8d4e420e9665826
Kai Hendry <hendry@iki.fi>
2017-03-31 22:40:58 +0800

    validate later

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const name = process.argv[2]
 const url = process.argv[3]
 
 feedparser.parse(url).then((items) => {
-  console.log(`<div id=${name}><ul>`)
+  console.log(`<div id=${name}>`)
   switch (name) {
     case 'youtube':
       console.log('<a href="https://www.youtube.com/user/kaihendry?sub_confirmation=1"><h2>Tech channel</h2></a>')
@@ -14,8 +14,9 @@ feedparser.parse(url).then((items) => {
       break
     default:
   }
+  console.log('<ul>')
   items.forEach((item) => {
-    // console.log(item)
+    // TODO: Quote the link to pass https://validator.w3.org/nu/?doc=https%3A%2F%2Fhendry.iki.fi%2F
     console.log(`<li><a href=${item.link}>${item.title}</a></li>`)
   })
   console.log('</ul></div>')
